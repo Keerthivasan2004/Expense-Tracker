ddeb2bd3d43c4ab0cb0c916548980f89835faa78
Keerthivasan <skeerthivasan044@email.com>
2026-09-03 06:59:01 +0530

    fix: hardcode Render API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://expense-tracker-1ri2.onrender.com/api'
+  baseURL: 'https://expense-tracker-1ri2.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
